84b574c5a368de9cb3db765a6a6f5523d3e35ad7
Foyez-Ahamed <foyez1435@gmail.com>
2023-12-14 02:53:26 +0600

    send user data to database

--- index.js
+++ index.js
@@ -27,7 +27,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+    
     await client.connect();
+
+    const usersCollection = client.db('libraryManagement').collection('users');
     
     const booksCategoryCollection = client.db('libraryManagement').collection('booksCategory');
 
@@ -37,6 +40,17 @@ async function run() {
     
 
     
+    app.post("/createUser", async (req, res) => {
+      const users = req.body;
+      const query = { email: users.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exist", insertedId: null });
+      }
+      const result = await usersCollection.insertOne(users);
+      res.send(result);
+    });
+
     
     app.post('/createBooks', async(req, res) => {
         const addBooks = req.body;
@@ -237,6 +251,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
